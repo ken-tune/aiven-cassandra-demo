817c3abfb79cd6dc15003dc76d9146230eda79e8
vi !$ <ken.tune@aiven.io>
2024-02-08 16:43:21 +0000

    Make use of TF_VAR to pass through environment variables to terraform where needed

--- env.sh
+++ env.sh
@@ -2,17 +2,20 @@
 
 source userSpecific.sh
 
+# Default cassandra user name
+AIVEN_ADMIN_USER=avnadmin
+
+# Service name prefix
+SERVICE_NAME_PREFIX=aiven-demo
+
 # Cassandra root directory - needed for cassandra-stress and cqlsh
-CASSANDRA_TOOLS_DIR=../downloads/apache-cassandra-4.0.12
+CASSANDRA_TOOLS_DIR=apache-cassandra-4.0.12
 
 # Default Cassandra port on Aiven
 CASSANDRA_PORT=13091
 
 # Cassandra host name
-CASSANDRA_HOST=callsign-cassandra-${AIVEN_PROJECT}.a.aivencloud.com
-
-# Default cassandra user name
-AIVEN_ADMIN_USER=avnadmin
+CASSANDRA_HOST=${SERVICE_NAME_PREFIX}-cassandra-${AIVEN_PROJECT}.a.aivencloud.com
 
 # CA certificate details
 CERTS_DIR=certs
@@ -26,3 +29,8 @@ export SSL_CERTFILE=$CA_PATH
 
 # Put cqlsh and cassandra-stress in path
 PATH=$PATH:$CASSANDRA_TOOLS_DIR/tools/bin:$CASSANDRA_TOOLS_DIR/bin
+
+# Need to export some variables to Terraform so they can be used
+export TF_VAR_project_name=$AIVEN_PROJECT
+export TV_VAR_service_name_prefix=$SERVICE_NAME_PREFIX
+
