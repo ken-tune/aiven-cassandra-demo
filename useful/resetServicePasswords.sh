8f6d119e96ab5f8bf3613799f551568ba2157387
vi !$ <ken.tune@aiven.io>
2024-02-08 17:03:01 +0000

    Service name prefix in reset passwords script

--- useful/resetServicePasswords.sh
+++ useful/resetServicePasswords.sh
@@ -2,6 +2,6 @@
 
 source env.sh
 
-avn service user-password-reset callsign-cassandra --project $AIVEN_PROJECT --username $AIVEN_ADMIN_USER --new-password $AIVEN_SERVICE_PASSWORD
-avn service user-password-reset callsign-cassandra-logs --project $AIVEN_PROJECT --username $AIVEN_ADMIN_USER --new-password $AIVEN_SERVICE_PASSWORD
-avn service user-password-reset callsign-cassandra-grafana --project $AIVEN_PROJECT --username $AIVEN_ADMIN_USER --new-password $AIVEN_SERVICE_PASSWORD
+avn service user-password-reset ${SERVICE_NAME_PREFIX}-cassandra --project $AIVEN_PROJECT --username $AIVEN_ADMIN_USER --new-password $AIVEN_SERVICE_PASSWORD
+avn service user-password-reset ${SERVICE_NAME_PREFIX}-cassandra-logs --project $AIVEN_PROJECT --username $AIVEN_ADMIN_USER --new-password $AIVEN_SERVICE_PASSWORD
+avn service user-password-reset ${SERVICE_NAME_PREFIX}-cassandra-grafana --project $AIVEN_PROJECT --username $AIVEN_ADMIN_USER --new-password $AIVEN_SERVICE_PASSWORD
