e1b0792b08df837dedbd87f6344a07b1285a1c1c
vi !$ <ken.tune@aiven.io>
2024-02-08 19:31:06 +0000

    Minor mod to get port

--- run-benchmarker.sh
+++ run-benchmarker.sh
@@ -7,7 +7,7 @@
 useful/check-certs.sh
 
 # Get Cassandra port
-useful/get-cassandra-port.sh
+. useful/get-cassandra-port.sh
 
 cassandra-stress write n=100k -mode native cql3 user=$AIVEN_ADMIN_USER password=$AIVEN_SERVICE_PASSWORD -node $CASSANDRA_HOST:$CASSANDRA_PORT  \
 -transport "truststore=$TRUSTSTORE_LOCATION truststore-password=$TRUSTSTORE_PASSWORD"
--- run-cql.sh
+++ run-cql.sh
@@ -7,6 +7,7 @@
 useful/check-certs.sh
 
 # Get Cassandra port
-useful/get-cassandra-port.sh
+. useful/get-cassandra-port.sh
 
+echo $CASSANDRA_PORT
 cqlsh --ssl -u $AIVEN_ADMIN_USER -p $AIVEN_SERVICE_PASSWORD $CASSANDRA_HOST $CASSANDRA_PORT
