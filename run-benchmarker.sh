f3d13dad71a58c773b5164e3ac0e999633e58c99
vi !$ <ken.tune@aiven.io>
2024-02-23 18:54:16 +0000

    More flags needed for benchmarker

--- run-benchmarker.sh
+++ run-benchmarker.sh
@@ -9,5 +9,11 @@ useful/check-certs.sh
 # Get Cassandra port
 . useful/get-cassandra-port.sh
 
-cassandra-stress write n=100k -mode native cql3 user=$AIVEN_ADMIN_USER password=$AIVEN_SERVICE_PASSWORD -node $CASSANDRA_HOST:$CASSANDRA_PORT  \
+CONSISTENCY_LEVEL=QUORUM
+RECORD_COUNT=100m
+REPLICATION_FACTOR=3
+THREADS=16
+
+cassandra-stress write no-warmup cl=$CONSISTENCY_LEVEL  n=$RECORD_COUNT -schema "replication(strategy=SimpleStrategy,factor=$REPLICATION_FACTOR)" -mode native cql3 \
+user=$AIVEN_ADMIN_USER password=$AIVEN_SERVICE_PASSWORD -node $CASSANDRA_HOST:$CASSANDRA_PORT -rate threads=$THREADS \
 -transport "truststore=$TRUSTSTORE_LOCATION truststore-password=$TRUSTSTORE_PASSWORD"
