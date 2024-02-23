1f607675b61bb73abc179575f990f31d81abf1c9
vi !$ <ken.tune@aiven.io>
2024-02-23 15:17:33 +0000

    Make example password something that doesn't look like a password

--- env.sh
+++ env.sh
@@ -19,7 +19,7 @@ CERTS_DIR=certs
 
 # Needed in a truststore for cassandra-stress
 TRUSTSTORE_LOCATION=$CERTS_DIR/myTrustStore.jks
-TRUSTSTORE_PASSWORD=MLGnUg3c4
+TRUSTSTORE_PASSWORD=my_truststore_password
 
 # And exported as SSL_CERTFILE for cqlsh
 CA_PATH=$CERTS_DIR/ca.pem
